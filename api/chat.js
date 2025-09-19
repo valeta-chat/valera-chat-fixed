f8c90760024d2e6fe462c76add5ea809d59badee
valeta-chat <maxsafon@gmail.com>
2025-09-19 15:15:02 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -9,7 +9,7 @@ function findCategory(query) {
 }
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).end();
+  if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
   const { messages } = req.body;
 
   const userMessage = messages[messages.length - 1].content;
